c3c0decc83c4d6c94d2c448391fc8dd51b13f3d9
William Casarin <jb55@jb55.com>
2025-06-30 11:57:28 -0700

    input: add set_ime_editor_info
    This corresponds to the GameActivity_setImeEditorInfo function on
    GameActivity. This is not supported on NativeActivity.
    
    Signed-off-by: William Casarin <jb55@jb55.com>

--- android-activity/src/native_activity/mod.rs
+++ android-activity/src/native_activity/mod.rs
@@ -18,7 +18,8 @@ use crate::input::{Axis, KeyCharacterMap, KeyCharacterMapBinding};
 use crate::input::{TextInputState, TextSpan};
 use crate::jni_utils::{self, CloneJavaVM};
 use crate::{
-    util, AndroidApp, ConfigurationRef, InputStatus, MainEvent, PollEvent, Rect, WindowManagerFlags,
+    AndroidApp, ConfigurationRef, ImeOptions, InputStatus, InputType, MainEvent, PollEvent, Rect,
+    WindowManagerFlags, util,
 };
 
 pub mod input;
@@ -390,6 +391,11 @@ impl AndroidAppInner {
 
     // TODO: move into a trait
     pub fn set_text_input_state(&self, _state: TextInputState) {
+        // NOP: Unsupported
+    }
+
+    // TODO: move into a trait
+    pub fn set_ime_editor_info(&self, _input_type: InputType, _options: ImeOptions) {
         // NOP: Unsupported
     }
 
